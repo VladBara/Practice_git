eb8aff1c50ece7ad03165a084608cca0e951c242
Vlad <vladbara@yahoo.com>
2022-10-03 17:07:54 +0300

    4 baba

--- code_1.c
+++ code_1.c
@@ -32,17 +32,18 @@
 #include<stdio.h>    
 
 int main(){    
-   int x=0,y=0,result=0;  
+  int x=0,y=0,result=0;  
+  
+  printf("enter first number:");  
+  scanf("%d",&x);  
+  printf("enter second number:");  
+  scanf("%d",&y);  
+  
+  result=x+y;  
+  printf("sum of 2 numbers:%d ",result);  
    
-   printf("enter first number:");  
-   scanf("%d",&x);  
-   printf("enter second number:");  
-   scanf("%d",&y);  
-   
-   result=x+y;  
-   printf("sum of 2 numbers:%d ",result);  
-   
-  printf("shdj sdfjjdsf jjf ");  
+  printf("ana are mrere ");  
+  printf("johhny ffs  ");  
 
    return 0;  
 }    
