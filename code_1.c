c1a653f474c3dc967a62d032717beca1c261d0b2
Vlad <vladbara@yahoo.com>
2022-10-03 15:31:34 +0300

    2 baba

--- code_1.c
+++ code_1.c
@@ -1,46 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <pthread.h>
 
-// Global variable:
-int i = 2;
+// // Global variable:
+// int i = 2;
 
-void* foo(void* p){
-  // Print value received as argument:
-  printf("Value recevied as argument in starting routine: ");
-  printf("%i\n", * (int*)p);
+// void* foo(void* p){
+//   // Print value received as argument:
+//   printf("Value recevied as argument in starting routine: ");
+//   printf("%i\n", * (int*)p);
 
-  // Return reference to global variable:
-  pthread_exit(&i);
-}
+//   // Return reference to global variable:
+//   pthread_exit(&i);
+// }
 
-int main(void){
-  // Declare variable for thread's ID:
-  pthread_t id;
+// int main(void){
+//   // Declare variable for thread's ID:
+//   pthread_t id;
 
-  int j = 1;
-  pthread_create(&id, NULL, foo, &j);
+//   int j = 1;
+//   pthread_create(&id, NULL, foo, &j);
     
-  int* ptr;
+//   int* ptr;
 
-  // Wait for foo() and retrieve value in ptr;
-  pthread_join(id, (void**)&ptr);
-  printf("Value recevied by parent from child: ");
-  printf("%i\n", *ptr);
-}
+//   // Wait for foo() and retrieve value in ptr;
+//   pthread_join(id, (void**)&ptr);
+//   printf("Value recevied by parent from child: ");
+//   printf("%i\n", *ptr);
+// }
 
-// #include<stdio.h>    
+#include<stdio.h>    
 
-// int main(){    
-//    int x=0,y=0,result=0;  
+int main(){    
+   int x=0,y=0,result=0;  
    
-//    printf("enter first number:");  
-//    scanf("%d",&x);  
-//    printf("enter second number:");  
-//    scanf("%d",&y);  
+   printf("enter first number:");  
+   scanf("%d",&x);  
+   printf("enter second number:");  
+   scanf("%d",&y);  
    
-//    result=x+y;  
-//    printf("sum of 2 numbers:%d ",result);  
+   result=x+y;  
+   printf("sum of 2 numbers:%d ",result);  
    
-//    return 0;  
-// }    
+   return 0;  
+}    
